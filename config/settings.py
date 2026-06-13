93e7f08e1f280dbe61c9cf59d7c8278a1c1be9c8
w31come <realgashoff@yandex.ru>
2026-06-13 05:45:16 +0300

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+# Настройки скрипта
+
+# VK API
+API_VERSION = '5.199'
+
+# Задержки (в секундах)
+DELAY_BETWEEN_POSTS = 15      # Между постами
+CYCLE_PAUSE = 15 * 60         # Между циклами = 15 минут
+
+# Gist
+GIST_FILENAME = 'vk_token.json'
+
+# Логирование
+LOG_FORMAT = '[{time}] {msg}'
